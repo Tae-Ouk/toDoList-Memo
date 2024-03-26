10c6008855a639b120b1dc444713ba61162d4fe8
Tae-Ouk <to99820@naver.com>
2024-03-26 18:04:00 +0900

    toDoList Memo App Lv.1

--- toDoList/ViewController.swift
+++ toDoList/ViewController.swift
@@ -1,19 +1,45 @@
 //
 //  ViewController.swift
-//  toDoList
+//  number BaseBall App
 //
-//  Created by TaeOuk Hwang on 3/26/24.
+//  Created by TaeOuk Hwang on 3/20/24.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var toDoListTable: UITableView!
+    
+    var data: [(id: Int, title: String, isCompleted: Bool)] = []
+    var count: Int = 0
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        toDoListTable.dataSource = self
+        toDoListTable.delegate = self
+        toDoListTable.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
     }
-
-
 }
 
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        let item = data[indexPath.row]
+        cell.textLabel?.text = item.title
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("Selected: \(data[indexPath.row])")
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
